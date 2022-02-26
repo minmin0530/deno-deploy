e434e86a730edb77d83450f419335e64f6311317
Yoshiki Izumi <izumiyoshiki@icloud.com>
2022-02-26 12:17:35 +0900

    correction japanese

--- hello.ts
+++ hello.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 function handler(_req: Request): Response {
-  return new Response("Hello, World");
+  return new Response("continue-jump を deno で構築中！");
 }
 
 console.log("Listening on http://localhost:8000");
