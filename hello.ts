e6c5dd9b7899804e9350d25ec66f32a04d5523c6
Yoshiki Izumi <izumiyoshiki@icloud.com>
2022-03-07 19:20:41 +0900

    add file

--- hello.ts
+++ hello.ts
@@ -1,9 +1,49 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import * as path from "https://deno.land/std@0.128.0/path/mod.ts";
+import { readableStreamFromReader } from "https://deno.land/std@0.128.0/streams/mod.ts";
 
-function handler(_req: Request): Response {
-  return new Response("continue-jump を deno で構築中！");
+// Start listening on port 8080 of localhost.
+const server = Deno.listen({ port: 8080 });
+console.log("File server running on http://localhost:8080/");
+
+for await (const conn of server) {
+  handleHttp(conn);
 }
 
-console.log("Listening on http://localhost:8000");
-serve(handler);
+async function handleHttp(conn: Deno.Conn) {
+  const httpConn = Deno.serveHttp(conn);
+  for await (const requestEvent of httpConn) {
+    // Use the request pathname as filepath
+    const url = new URL(requestEvent.request.url);
+    const filepath = decodeURIComponent(url.pathname);
+
+    // Try opening the file
+    let file;
+    try {
+      file = await Deno.open("." + filepath, { read: true });
+      const stat = await file.stat();
+
+      // If File instance is a directory, lookup for an index.html
+      if (stat.isDirectory) {
+        file.close();
+        const filePath = path.join("./", filepath, "index.html");
+        file = await Deno.open(filePath, { read: true });
+      }
+    } catch {
+      // If the file cannot be opened, return a "404 Not Found" response
+      const notFoundResponse = new Response("404 Not Found", { status: 404 });
+      await requestEvent.respondWith(notFoundResponse);
+      return;
+    }
+
+    // Build a readable stream so the file doesn't have to be fully loaded into
+    // memory while we send it
+    const readableStream = readableStreamFromReader(file);
+
+    // Build and send the response
+    const response = new Response(readableStream);
+    await requestEvent.respondWith(response);
+  }
+}
+
 
