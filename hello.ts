3bfa8c002fc9bac08f35d623c1c8e85880e9dc02
Yoshiki Izumi <izumiyoshiki@icloud.com>
2022-09-09 02:12:23 +0900

    Update hello.ts

--- hello.ts
+++ hello.ts
@@ -1,10 +1,10 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import { readableStreamFromReader } from "https://deno.land/std@0.128.0/streams/mod.ts";
 import{ MongoClient } from "https://deno.land/x/mongo@v0.30.1/mod.ts";
-const client = new MongoClient();
-await client.connect(
-  "mongodb://mongo:password@133.167.44.238:27017/?authSource=othello223&readPreference=primary&ssl=false&directConnection=true",
-);
+//const client = new MongoClient();
+//await client.connect(
+//  "mongodb://mongo:password@133.167.44.238:27017/?authSource=othello223&readPreference=primary&ssl=false&directConnection=true",
+//);
 interface Account {
   _id: ObjectId;
   name: string;
@@ -34,10 +34,10 @@ async function handleRequest(request: Request): Promise<Response> {
     });
   } else {
     if (filepath == "/get_account") {
-      const db = client.database("othello223");
-      const account = db.collection<Account>("account");
-      const all_users = await account.find({ name: { $ne: null } }).toArray();
-      const body = JSON.stringify(all_users[0], null, 2);
+//      const db = client.database("othello223");
+//      const account = db.collection<Account>("account");
+//      const all_users = await account.find({ name: { $ne: null } }).toArray();
+      const body = JSON.stringify({"name":"yoshiki"}, null, 2);
       return new Response(body, {
         headers: { "content-type": "application/json; charset=utf-8" },
       });
